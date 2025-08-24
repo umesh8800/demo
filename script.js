e3317aa27b7e60070a6bc756aa086beb143e4df7
Umesh Shakya <umesh981131@gmail.com>
2025-08-24 06:20:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// // What is Map()
+// let arr = [1, 2, 3, 4, 5];
+// let mapResult = arr.map((num) => num * 2);  
+// console.log("Map Result:", mapResult); // [2, 4, 6, 8, 10]
+
+// // What is Filter()
+// let filterResult = arr.filter((num) => num % 2 === 0);  
+// console.log("Filter Result:", filterResult); // [2, 4]
+
+// // What is Reduce()
+// let reduceResult = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+// console.log("Reduce Result:", reduceResult); // 15
+
+
+let students = [
+    { name: "Alice", age: 20, grade: 85 },  
+    { name: "Bob", age: 22, grade: 90 },    
+    { name: "Charlie", age: 23, grade: 95 },
+    { name: "David", age: 21, grade: 88 }];
+
+   let morethan20 = students.filter(student => student.age > 20).map(student => student.name);
+   console.log(morethan20);
